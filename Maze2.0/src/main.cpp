850ac93f296b20ad0301a450aeec5ecbc171645f
Sivi2706 <137089663+Sivi2706@users.noreply.github.com>
2025-02-15 22:06:28 +0800

    encoder

--- Maze2.0/src/main.cpp
+++ Maze2.0/src/main.cpp
@@ -1,15 +1,12 @@
 #include "NewPing.h"
 
-// Hook up HC-SR04 with Trig to Arduino Pin A4, Echo to Arduino Pin A5
-#define TRIGGER_PIN A5
-#define ECHO_PIN A4 
-
+// Ultrasonic Sensor Pins
 #define FRONT_TRIGGER_PIN A5
 #define FRONT_ECHO_PIN A4
-#define LEFT_TRIGGER_PIN
-#define LEFT_ECHO_PIN
-#define RIGHT_TRIGGER_PIN
-#define RIGHT_ECHO_PIN
+#define LEFT_TRIGGER_PIN A3  // Define pins for left sensor
+#define LEFT_ECHO_PIN A2
+#define RIGHT_TRIGGER_PIN A1 // Define pins for right sensor
+#define RIGHT_ECHO_PIN A0
 
 // Motor Pins
 const int IN1 = 2;
@@ -20,13 +17,18 @@ const int FNA = 3;
 const int FNB = 11;
 
 // Maximum distance we want to ping for (in centimeters)
-#define MAX_DISTANCE 400	
+#define MAX_DISTANCE 400
 
-// NewPing setup of pins and maximum distance
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); //this line can be removed
+// NewPing objects for each ultrasonic sensor
 NewPing front(FRONT_TRIGGER_PIN, FRONT_ECHO_PIN, MAX_DISTANCE);
 NewPing left(LEFT_TRIGGER_PIN, LEFT_ECHO_PIN, MAX_DISTANCE);
 NewPing right(RIGHT_TRIGGER_PIN, RIGHT_ECHO_PIN, MAX_DISTANCE);
+
+// Rotary Encoder Variables
+volatile int count_l = 0; // Counter for rotary encoder
+
+// Function Prototype for Rotary Encoder ISR
+void counter();
 
 void setup() {
     Serial.begin(9600); // Initialize serial communication
@@ -38,6 +40,9 @@ void setup() {
     pinMode(IN4, OUTPUT);
     pinMode(FNA, OUTPUT);
     pinMode(FNB, OUTPUT);
+
+    // Rotary Encoder Setup
+    attachInterrupt(digitalPinToInterrupt(10), counter, RISING); // Attach interrupt for rotary encoder
 }
 
 void MoveForward(int PWM) {
@@ -74,53 +79,48 @@ void TurnRight() {
     digitalWrite(IN4, LOW);
 }
 
-void loop() {
-    int distance = sonar.ping_cm(); // Get distance in cm
+// Rotary Encoder Interrupt Service Routine
+void counter() {
+    count_l++; // Increment counter on each rising edge
+}
 
-    // Display distance in serial monitor
-    Serial.print("Distance = ");
-    Serial.print(distance);
+void loop() {
+    // Read distances from ultrasonic sensors
+    int frontDistance = front.ping_cm();
+    int leftDistance = left.ping_cm();
+    int rightDistance = right.ping_cm();
+
+    // Display distances in serial monitor
+    Serial.print("Front Distance = ");
+    Serial.print(frontDistance);
+    Serial.print(" cm | Left Distance = ");
+    Serial.print(leftDistance);
+    Serial.print(" cm | Right Distance = ");
+    Serial.print(rightDistance);
     Serial.println(" cm");
 
-    if (distance > 10 || distance == 0) { // Move forward if no obstacle or out of range
+    // Rotary Encoder Calculations
+    double var_1 = count_l / 20.0; // Convert counts to rotations
+    double var_2 = var_1 * 2 * 3.242 * 6.5; // Calculate distance traveled (example formula)
+    Serial.print("Distance Traveled = ");
+    Serial.print(var_2);
+    Serial.println(" cm");
+
+    // Obstacle Avoidance Logic
+    if (frontDistance > 10 || frontDistance == 0) { // Move forward if no obstacle in front
         Serial.println("Moving forward");
         MoveForward(150); // Set motor speed to 150
-    } else {
-        Serial.println("Object detected!");
-        Stop(); // Stop the car if object is within 10 cm
-
-        // Use a while loop to ensure the car remains stopped
-        while (distance <= 15 && distance != 0) {
-            distance = sonar.ping_cm(); // Recheck the distance
-            Serial.print("Distance: ");
-            Serial.print(distance);
-            Serial.println(" cm");
-
-            Serial.println("Object detected!");
-            delay(100); // Short delay to prevent rapid looping
-        }
+    } else if (leftDistance > rightDistance) { // Turn left if more space on the left
+        Serial.println("Turning left");
+        TurnLeft();
+        delay(500); // Turn for a short duration
+        Stop();
+    } else { // Turn right if more space on the right
+        Serial.println("Turning right");
+        TurnRight();
+        delay(500); // Turn for a short duration
+        Stop();
     }
+
     delay(100); // Short delay for smoother operation
 }
-
-
-
-
-
-
-/*
-For the rotary encoder part
-
-void setup() {
-...
-
-  attachPCINT(10, counter, RISING);
-}
-
-
-void loop() {
-...
-
-  double var_1 = count_l/20;
-  double var_2 = var_1*2*3.242*6.5;}
-*/
