ae4fcc64d24a8d37416c1ea267ea8b4437e40348
Sivi2706 <137089663+Sivi2706@users.noreply.github.com>
2025-02-15 21:29:04 +0800

    Ultrasonic+Motor

--- Maze2.0/src/main.cpp
+++ Maze2.0/src/main.cpp
@@ -1,23 +1,16 @@
 #include "NewPing.h"
-#include <LiquidCrystal.h>
-
-//hi there 
 
 // Hook up HC-SR04 with Trig to Arduino Pin A4, Echo to Arduino Pin A5
 #define TRIGGER_PIN A5
 #define ECHO_PIN A4 
 
 // Motor Pins
-const int IN1 = 1;
-const int IN2 = 10;
-const int IN3 = 12;
-const int IN4 = 13;
+const int IN1 = 2;
+const int IN2 = 4;
+const int IN3 = 5;
+const int IN4 = 6;
 const int FNA = 3;
 const int FNB = 11;
-
-// LCD Pins
-const int rs = 8, en = 9, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 // Maximum distance we want to ping for (in centimeters)
 #define MAX_DISTANCE 400	
@@ -26,7 +19,7 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
 void setup() {
-	Serial.begin(9600);
+    Serial.begin(9600); // Initialize serial communication
 
     // Motor Pin Setup
     pinMode(IN1, OUTPUT);
@@ -35,12 +28,6 @@ void setup() {
     pinMode(IN4, OUTPUT);
     pinMode(FNA, OUTPUT);
     pinMode(FNB, OUTPUT);
-
-    // LCD initialization
-    lcd.begin(16, 2);
-    lcd.print("Initializing...");
-    delay(1000);
-    lcd.clear();
 }
 
 void MoveForward(int PWM) {
@@ -61,34 +48,27 @@ void Stop() {
 
 void loop() {
     int distance = sonar.ping_cm(); // Get distance in cm
+
+    // Display distance in serial monitor
     Serial.print("Distance = ");
     Serial.print(distance);
     Serial.println(" cm");
 
-    lcd.setCursor(0, 0);
-    lcd.print("Distance: ");
-    lcd.print(distance);
-    lcd.print(" cm  ");
-
     if (distance > 10 || distance == 0) { // Move forward if no obstacle or out of range
-        lcd.setCursor(0, 1);
-        lcd.print("Moving forward");
+        Serial.println("Moving forward");
         MoveForward(150); // Set motor speed to 150
     } else {
-        lcd.setCursor(0, 1);
-        lcd.print("Object detected!");
+        Serial.println("Object detected!");
         Stop(); // Stop the car if object is within 10 cm
 
         // Use a while loop to ensure the car remains stopped
         while (distance <= 15 && distance != 0) {
             distance = sonar.ping_cm(); // Recheck the distance
-            lcd.setCursor(0, 0);
-            lcd.print("Distance: ");
-            lcd.print(distance);
-            lcd.print(" cm  ");
+            Serial.print("Distance: ");
+            Serial.print(distance);
+            Serial.println(" cm");
 
-            lcd.setCursor(0, 1);
-            lcd.print("Object detected!");
+            Serial.println("Object detected!");
             delay(100); // Short delay to prevent rapid looping
         }
     }
