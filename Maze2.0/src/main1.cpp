e7a23faf2c716262d5c099b0d83839e2e4c0969e
Nath <nathanaelng05@gmail.com>
2025-03-10 18:13:20 +0800

    SUIUIIIIIIIII

--- Maze2.0/src/main1.cpp
+++ Maze2.0/src/main1.cpp
@@ -15,12 +15,24 @@
 #define RIGHT_TRIGGER_PIN A0
 #define RIGHT_ECHO_PIN A1
 
+// Motor Pins
+#define IN1 2
+#define IN2 4
+#define IN3 5
+#define IN4 6
+#define FNA 3
+#define FNB 11
+
 char movement_arr[SIZE];
 int junction_nodes[SIZE];
 int junction_visited[SIZE];
 int index = 0;
 int count = 0;
 
+void ultrasonicSetup(int trigPin, int echoPin) {
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+}
 
 float getDistance(int trigPin, int echoPin) {
     digitalWrite(trigPin, LOW);
@@ -58,7 +70,7 @@ void move_forward()
         Serial.println(i);
     }
 
-    delay(10000);
+    delay(7000);
 }
 
 void turn_left_90()
@@ -70,7 +82,7 @@ void turn_left_90()
         Serial.println(i);
     }
 
-    delay(10000);
+    delay(7000);
 }
 
 void turn_right_90()
@@ -82,7 +94,7 @@ void turn_right_90()
         Serial.println(i);
     }
 
-    delay(10000);
+    delay(7000);
 }
 
 void turn_180()
@@ -94,18 +106,20 @@ void turn_180()
         Serial.println(i);
     }
 
-    delay(10000);
+    delay(7000);
 }
 
 void backtrack()
 {
-    turn_180();
-    delay(10000);
-
     Serial.println("Backtrack starting.");
+    turn_180();
+    delay(5000);
+
 
     for (count -= 1; count > junction_nodes[index]; count--)
     {
+        Serial.print("Count: "); Serial.println(count);
+
         if (movement_arr[count] == 'F') 
         {
             Serial.println("Move forward now");
@@ -122,6 +136,13 @@ void backtrack()
             turn_left_90();
         }
     }    
+
+    if (movement_arr[junction_nodes[index]] == 'F')
+    {
+        Serial.println("Move forward now");
+        move_forward();
+    }
+    count--;
 }
 
 void search_maze()
@@ -138,10 +159,18 @@ void search_maze()
     Serial.print("Front: "); Serial.println(front); 
     Serial.print("Left: "); Serial.println(left);
     Serial.print("Right: "); Serial.println(right);
+    Serial.println("-----");
+    Serial.println(movement_arr);
+    Serial.println("-----");
+    
+    Serial.println("-----");
+    Serial.print("Index: "); Serial.println(index);
+    Serial.print("Junction visited: "); Serial.println(junction_visited[index]);
+    Serial.println("-----");
 
     delay(5000);
 
-    if (front == 1 && junction_nodes[index] < 1)                 // front has space
+    if (front == 1 && (count != junction_nodes[index] || junction_visited[index] < 1))                 // front has space
     {
         Serial.println("Front space detected");
         if (left == 1 || right == 1)
@@ -159,7 +188,7 @@ void search_maze()
         count++;
         Serial.println("Forward movement stored.");
     }
-    else if (left == 1 && junction_nodes[index] < 2)             // left has space
+    else if (left == 1 && (count != junction_nodes[index] || junction_visited[index] < 2))             // left has space
     {
         Serial.println("Left space detected");
         if (right == 1)
@@ -184,7 +213,7 @@ void search_maze()
         count++;
         Serial.println("Forward movement stored.");
     }
-    else if (right == 1 && junction_nodes[index] < 3)           // right has space
+    else if (right == 1 && (count != junction_nodes[index] || junction_visited[index] < 3))           // right has space
     {
         Serial.println("Right space detected");
         
@@ -225,16 +254,16 @@ void search_maze()
         }
         else if (movement_arr[count] == 'L')
         {
-            Serial.println("Turn right now");
-            turn_right_90();
+            Serial.println("Turn left now");
+            turn_left_90();
             Serial.println("Reorienting: L (Left)");
             junction_visited[index] = 2;
             Serial.println("Junction visited stored as 2.");
         }
         else if (movement_arr[count] == 'R')
         {
-            Serial.println("Turn left now");
-            turn_left_90();
+            Serial.println("Turn right now");
+            turn_right_90();
             Serial.println("Reorienting: R (Right)");
             junction_visited[index] = 3;
             Serial.println("Junction visited stored as 3.");
@@ -254,6 +283,17 @@ void init_arrays()
 void setup() 
 {
     Serial.begin(9600);
+
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+    pinMode(IN3, OUTPUT);
+    pinMode(IN4, OUTPUT);
+    pinMode(FNA, OUTPUT);
+    pinMode(FNB, OUTPUT);
+    ultrasonicSetup(FRONT_TRIGGER_PIN, FRONT_ECHO_PIN);
+    ultrasonicSetup(LEFT_TRIGGER_PIN, LEFT_ECHO_PIN);
+    ultrasonicSetup(RIGHT_TRIGGER_PIN, RIGHT_ECHO_PIN);
+    Serial.println("Setup complete.");
 
     init_arrays();
 }
